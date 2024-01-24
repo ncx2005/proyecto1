2a261c87aac77ac040145ccdeba538fe456b27ab
ncx2005 <ncx2005@gmail.com>
2024-01-23 22:01:18 -0400

    Nelson

--- src/proyecto1/main.java
+++ src/proyecto1/main.java
@@ -16,6 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Proyecto 1 EDD");
+        System.out.println("Hola es nelson probando...");
     }
     
 }
